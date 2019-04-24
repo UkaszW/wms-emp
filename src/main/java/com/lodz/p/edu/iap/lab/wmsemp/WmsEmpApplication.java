b21772d221065566d27bce5d53c0a046567ae8bd
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-04-24 07:01:50 +0200

    added files

---
+++ src/main/java/com/lodz/p/edu/iap/lab/wmsemp/WmsEmpApplication.java
@@ -0,0 +1,13 @@
+package com.lodz.p.edu.iap.lab.wmsemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsEmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsEmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lodz/p/edu/iap/lab/wmsemp/WmsEmpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lodz.p.edu.iap.lab.wmsemp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WmsEmpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
