af7e2ee9482885531ccb724ecaf3669031db320c
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-04-24 10:04:02 +0200

    fix CROS

--- src/main/java/com/lodz/p/edu/iap/lab/wmsemp/ItemController.java
+++ src/main/java/com/lodz/p/edu/iap/lab/wmsemp/ItemController.java
@@ -1,5 +1,6 @@
 package com.lodz.p.edu.iap.lab.wmsemp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,8 @@ public class ItemController {
         return "Server works ;)";
     }
 
-    @GetMapping("/items/all")
+    @GetMapping("/items/get/all")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Item> getAll() {
         return repository.findAll();
     }
