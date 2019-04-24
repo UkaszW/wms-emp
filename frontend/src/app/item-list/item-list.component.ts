5e0ff8a0c280879218245b822ffa141515704153
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-04-24 10:07:47 +0200

    added angular frontend

---
+++ frontend/src/app/item-list/item-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ItemService } from '../shared/item/item.service';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent implements OnInit {
+
+  items: Array<any>;
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit() {
+    this.itemService.getAll().subscribe(data => {
+      this.items = data;
+    })
+  }
+
+}
