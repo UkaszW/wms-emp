f3610e22cd708dbb479cf4d2b4e365cbe7137c38
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-07-03 15:29:22 +0200

    added data sync job

--- src/main/java/com/lodz/p/edu/iap/lab/wmsemp/job/DataSyncJob.java
+++ src/main/java/com/lodz/p/edu/iap/lab/wmsemp/job/DataSyncJob.java
@@ -1,25 +1,14 @@
 package com.lodz.p.edu.iap.lab.wmsemp.job;
 
-import com.lodz.p.edu.iap.lab.wmsemp.api.event.EventController;
-import com.lodz.p.edu.iap.lab.wmsemp.entity.BaseEntity;
-import com.lodz.p.edu.iap.lab.wmsemp.entity.event.Event;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.client.RestTemplate;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.time.Instant;
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -38,21 +27,20 @@ public class DataSyncJob {
     // "0 0 * * * *" every hour
     // "0 0/15 0 ? * *" every 15 minutes
     // "*/30 * * * * *" every 30 seconds
-    @Scheduled(cron = "*/30 * * * * *")
-    private void syncEvents() {
+    @Scheduled(cron = "0 0/15 0 ? * *")
+    private void syncData() {
         log.info("Date synchronization: " + Instant.now());
 
         try {
             Connection external = DriverManager.getConnection(DB_SYNC_URL,USER,PASS);
             Statement statement = external.createStatement();
-            String sql = "SCRIPT TO './backup.sql'";
-            statement.executeUpdate(sql);
+            String sql = "SCRIPT TO './src/main/resources/backup.sql'";
+            statement.execute(sql);
             statement.close();
             external.close();
 
-            jdbcTemplate.execute("RUNSCRIPT FROM 'backup.sql'");
-
-
+            jdbcTemplate.execute("DROP ALL OBJECTS");
+            jdbcTemplate.execute("RUNSCRIPT FROM './src/main/resources/backup.sql'");
         } catch (Exception e) {
             log.error("Error occurred: ", e);
         }
